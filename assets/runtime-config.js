50906c92ca0aaa3653a5809b267af10acc44a629
acecarmorj <271701030+acecarmorj@users.noreply.github.com>
2026-04-02 02:31:30 -0300

    ajusta base publica dinamica

--- assets/runtime-config.js
+++ assets/runtime-config.js
@@ -1,4 +1,15 @@
-window.ACS_RUNTIME_CONFIG = window.ACS_RUNTIME_CONFIG || {
-  API_URL:'https://script.google.com/macros/s/AKfycbwd7VGBUVHziARpZi6qvsqvYb-VtCPUDQlcvROopyEpKk8DpkE7qckTa5c7XS6TDelm/exec',
-  APP_BASE_URL: 'https://acecarmorj.github.io/codex/'
-};
+(function () {
+  var detectedBase = '';
+  try {
+    if (window.location && /^https?:/.test(window.location.protocol)) {
+      detectedBase = window.location.origin + window.location.pathname.replace(/[^/]*$/, '');
+    }
+  } catch (err) {
+    detectedBase = '';
+  }
+
+  window.ACS_RUNTIME_CONFIG = Object.assign({
+    API_URL:'https://script.google.com/macros/s/AKfycbwd7VGBUVHziARpZi6qvsqvYb-VtCPUDQlcvROopyEpKk8DpkE7qckTa5c7XS6TDelm/exec',
+    APP_BASE_URL: detectedBase
+  }, window.ACS_RUNTIME_CONFIG || {});
+})();
